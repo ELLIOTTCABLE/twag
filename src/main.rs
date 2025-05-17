3746faeb1fa2ba7e0912ebaa535d32bbf8281eb3
ELLIOTTCABLE <git@ell.io>
2025-05-17 14:27:23 -0500

    (re) Extract connection-validation to function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 use axum::{Router, routing::get};
-use sqlx::PgPool;
+use sqlx::{Error, PgPool, Pool, Postgres};
+
+async fn initialize_connection(database_url: &str) -> Result<Pool<Postgres>, Error> {
+   println!("Connecting to database at: {}", database_url);
+   let pool = PgPool::connect(database_url).await?;
+
+   sqlx::query("SELECT 1").fetch_one(&pool).await?;
+
+   Ok(pool)
+}
 
 #[tokio::main]
 async fn main() {
@@ -11,17 +20,9 @@ async fn main() {
 
    let database_url = dotenvy::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
-   println!("Connecting to database at: {}", database_url);
-   let pool = PgPool::connect(&database_url)
+   let pool = initialize_connection(&database_url)
       .await
       .expect("Failed to connect to database");
-
-   let row: (i32,) = sqlx::query_as("SELECT 1")
-      .fetch_one(&pool)
-      .await
-      .expect("Failed to execute query");
-
-   println!("Query result: {}", row.0);
 
    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
