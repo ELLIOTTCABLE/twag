f08fc516e4aae8be77540af6a3799cf686cbb37f
ELLIOTTCABLE <git@ell.io>
2025-05-17 14:33:07 -0500

    (- new dep) Use tracing package instead of println

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 use axum::{Router, routing::get};
 use sqlx::{Error, PgPool, Pool, Postgres};
+use tracing::{Level, info, trace};
 
 async fn initialize_connection(database_url: &str) -> Result<Pool<Postgres>, Error> {
-   println!("Connecting to database at: {}", database_url);
+   info!(database_url, "Connecting to database");
    let pool = PgPool::connect(database_url).await?;
 
    sqlx::query("SELECT 1").fetch_one(&pool).await?;
 
+   trace!("Connection established");
    Ok(pool)
 }
 
 #[tokio::main]
 async fn main() {
-   tracing_subscriber::fmt::init();
+   tracing_subscriber::fmt()
+      .event_format(tracing_subscriber::fmt::format().with_file(true).with_line_number(true))
+      .init();
 
    if dotenvy::from_filename(".env.local").is_err() {
       dotenvy::dotenv().ok();
@@ -27,5 +31,6 @@ async fn main() {
    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+   println!("Listening on http://{}", listener.local_addr().unwrap());
    axum::serve(listener, app).await.unwrap();
 }
